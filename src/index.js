07ddf73fd85aba7217c227cf5c9149b2f52f2a53
Ozzy Cavendish <martin.chris.wood@gmail.com>
2022-11-10 16:53:35 +0000

    Lifted state up to dice tray, multiple dice possible

--- src/index.js
+++ src/index.js
@@ -7,32 +7,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // N-sided dice
 class Dice extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sides: props.sides,
-      roll: 0,
-    };
-    if (props.startRoll) {
-      this.rollDice();
-    };
-  }
-  
-  // Change dice state at random
-  rollDice() {
-    this.setState({roll: (1 + Math.floor(Math.random() * this.state.sides))});
-  }
-
   // Creates single isolated canvas for dice
   render() {
     return (
-      <div id="dice_image">
-        <D6Graphic onClick = {() => this.rollDice()} rollnumber={this.state.roll}/>
-      </div>
+        <D6Graphic rollnumber={this.props.roll}
+        onClick={this.props.onClick}/>
     );
   }
 }
-
 
 // Tray to hold dice
 class DiceTray extends React.Component {
@@ -46,34 +28,61 @@ class DiceTray extends React.Component {
 
   addDice() {
     var tray = this.state.tray.slice();
-    this.setState({tray: tray.concat([Dice])});
+    var dice = {
+        dice_key: tray.length,
+        dice_sides: 6,
+        dice_roll: 0,
+    }
+    this.setState({tray: tray.concat([dice])});
+  }
+
+  rollOneDice(i) {
+    var tray = this.state.tray.slice();
+    var dice = tray[i];
+    dice['dice_roll'] = (1 + Math.floor(Math.random() * dice.dice_sides));
+    tray[i] = dice;
+    this.setState({tray: tray});
+  }
+
+  rollAllDice() {
+    var tray = this.state.tray.slice();
+    for (var i=0; i<tray.length; i++) {
+        var dice = tray[i];
+        dice['dice_roll'] = (1 + Math.floor(Math.random() * dice.dice_sides));
+        tray[i] = dice;
+    }
+    this.setState({tray: tray});
   }
 
   render() {
-    
     // Generate dice in tray
     const tray = this.state.tray;
-
     return (
       <div id="dice_tray">
         <div>
           <h2>Test tray: {this.state.tray.length}.</h2>
           <button onClick={() => this.addDice()}>Add Dice</button>
+          <button onClick={() => this.rollAllDice()}>Roll Dice</button>
+          <button onClick={() => this.setState({tray: []})}>Clear Dice</button>
         </div>
         <div id="dice">
-          {tray}
+          {tray.map((component) => (
+          <Dice 
+            key={component.dice_key} 
+            roll={component.dice_roll}
+            onClick={() => this.rollOneDice(component.dice_key)}/>))}
         </div>
       </div>
     );
   }
 }
 
+//  {tray.map((component) => (<React.Fragment key={component.dice_key}>{component.dice_roll}</React.Fragment>))}
 // Establish render to root
 const element = (
 <div>
   <h1>Roller</h1>
   <DiceTray />
-  <Dice sides = {30}/>
 </div>
 );
 
